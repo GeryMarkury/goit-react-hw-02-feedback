02551ea9138fcd4232f07a3c264f6ac03569cffa
Heorhii <gerythepatient@gmail.com>
2023-05-07 12:25:15 +0200

    Edit components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,55 @@
 import { Component } from 'react';
-import { SectionFeedbacks } from './SectionFeedbacks/SectionFeedbacks';
-import { SectionStats } from './SectionStats/SectionStats';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './Section/FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Section/Statistics/Statistics';
+import { Notification } from './Section/Notification/Notification';
+import css from '../components/App.module.css'
 
 export class App extends Component {
-  state = {
+  
+state = {
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
+    };
+ 
+  
+
+  onLeaveFeedback = (option) => {
+    this.setState({
+      [option]: this.state[option] + 1
+    })
   };
 
+  countTotalFeedbacks = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  countPositiveFeedbacksPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    const positivePercentage = total > 0 ? Math.round(((good + neutral) / total) * 100) : 0;
+    return positivePercentage;
+  }
+
   render() {
+    const { good, neutral, bad } = this.state;
     return (
-      <div>
-      <SectionFeedbacks title="Please leave feedback" />
-      <SectionStats title="Statistics" />
+      <div className={css.container}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={this.onLeaveFeedback}></FeedbackOptions>
+          </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedbacks() > 0 ? (
+            <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedbacks()} positivePercentage={this.countPositiveFeedbacksPercentage()}></Statistics>
+            ) : (
+            <Notification message="There is no feedback"></Notification>
+            )}
+            </Section>
       </div>
     )
   }
 };
 
-// створити мепом три кнопки за масивом в props options. всередині мепу кожній кнопці передаємо проп наприклад:
-// () => onLeaveFeedback(label тут буде гуд чи бед) i тоді setState буде {[option]: state[option] + 1} таким чином оновлювати state
-// в onLeaveFeedback передати метод, який отримуватиме одне зі значень (гуд або бед)
--- src/components/SectionStats/Statistics/Statistics.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react';
-
-export class Statistics extends Component {
-    render() {
-    return (
-        <div></div>
-    )
-  }
-};
